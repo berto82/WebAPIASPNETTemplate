502755bf6fc2f96990c50ba721f302cb37c60605
Roberto <r.faccani@outlook.it>
2024-08-13 09:15:30 +0200

    Creato progetto template

---
+++ WebAPIASPNETTemplate/Controllers/WeatherForecastController.vb
@@ -0,0 +1,28 @@
+﻿Namespace Controllers
+    <ApiController>
+    <Route("[controller]")>
+    Public Class WeatherForecastController
+        Inherits ControllerBase
+
+        Private ReadOnly Summaries() As String = New String() {"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"}
+
+        Private ReadOnly _logger As ILogger(Of WeatherForecastController)
+
+        Public Sub New(logger As ILogger(Of WeatherForecastController))
+            _logger = logger
+        End Sub
+
+
+        <HttpGet(Name:="GetWeatherForecast")>
+        Public Function [Get]() As IEnumerable(Of WeatherForecast)
+
+            Return Enumerable.Range(1, 5).Select(Function(index) New WeatherForecast With {
+                   .Date = DateOnly.FromDateTime(Date.Now.AddDays(index)),
+                   .TemperatureC = New Random().Next(-20, 55),
+                   .Summary = Summaries(New Random().Next(Summaries.Length))
+               }).ToArray()
+
+        End Function
+
+    End Class
+End Namespace
--- WebAPIASPNETTemplate/Program.vb
+++ WebAPIASPNETTemplate/Program.vb
@@ -1,7 +1,30 @@
-Imports System
-
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+
+        Dim builder = WebApplication.CreateBuilder(args)
+
+        'Add services to the container.
+
+        builder.Services.AddControllers()
+        'Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer()
+        builder.Services.AddSwaggerGen()
+
+        Dim app = builder.Build()
+
+        'Configure the HTTP request pipeline.
+        If app.Environment.IsDevelopment() Then
+            app.UseSwagger()
+            app.UseSwaggerUI()
+        End If
+
+        app.UseHttpsRedirection()
+
+        app.UseAuthorization()
+
+        app.MapControllers()
+
+        app.Run()
+
     End Sub
 End Module
